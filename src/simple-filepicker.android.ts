8b336cd21f0e2f2f9b1d5f9085b41547df068ecd
Robert Keller <robertkeller04@gmail.com>
2020-11-17 08:16:04 +0100

    Added MimeType fix according to https://github.com/bliulin/nativescript-simple-filepicker/commit/b6d08d56443c5d9d02c565631704df54b7bc5f06

--- src/simple-filepicker.android.ts
+++ src/simple-filepicker.android.ts
@@ -19,29 +19,20 @@ function callIntent(context, intent, pickerType) {
 }
 
 function setMimeTypeOnIntent(intent: any, allowedTypes: string[]): void {
-    const types = allowedTypes || [];
-    if (types.length === 0) {
-        // When there is no intent then assume all
-        intent.setType("*/*");
+    if (allowedTypes.length === 0) {
         return;
     }
-    const extensions = Array.create(java.lang.String, types.length);
-    for (let i = 0; i < types.length; i++) {
-        extensions[i] = types[i];
+
+    const extensions = Array.create(java.lang.String, allowedTypes.length);
+    for (let i = 0; i < allowedTypes.length; i++) {
+        extensions[i] = allowedTypes[i];
     }
     if (extensions.length > 1) {
-        // When there is multiple intent then use these extras
-        // intent.setType("*/*");
+        intent.setType("*/*");
         intent.putExtra(android.content.Intent.EXTRA_MIME_TYPES, extensions);
     }
     else {
-        if (extensions.length === 1) {
-            // When there is a single intent then use this one
-            intent.setType(extensions[0]);
-        } else {
-            // When there is no intent then assume all
-            intent.setType("*/*");
-        }
+        intent.setType(extensions[0]);
     }
 }
 
@@ -55,9 +46,7 @@ export const openFilePicker = (params?: FilePickerOptions) => {
     intent.putExtra(android.content.Intent.EXTRA_ALLOW_MULTIPLE, params && !!params.multipleSelection || false);
 
     const allowedTypes = params ? params.extensions : [];
-    // setMimeTypeOnIntent(intent, allowedTypes);
-    intent.setType('image/*|application/pdf');
-
+    setMimeTypeOnIntent(intent, allowedTypes);
     return callIntent(context, intent, FILE_CODE).then((result: any) => {
         if (result.resultCode === android.app.Activity.RESULT_OK) {
             if (result.intent != null) {
